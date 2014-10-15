49a56245934b84226206887b72516bc3ae514fdd
jjhlzn <jinjunhang@hotmail.com>
2014-10-15 22:45:39 +0800

    部署文件: unicorn & nginx

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/jjh/mahjong_account"
+pid "/home/jjh/mahjong_account/tmp/pids/unicorn.pid"
+stderr_path "/home/jjh/mahjong_account/log/unicorn.log"
+stdout_path "/home/jjh/mahjong_account/log/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 2
+timeout 30
